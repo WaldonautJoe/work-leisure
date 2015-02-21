f592bc7b670832bc7876c9d0e8833347ec2c026e
WaldonautJoe <omanacent@gmail.com>
2015-02-21 06:44:54 -0600

    Display same info in Late/Upcoming Tasks Activity as Task List Activity
    - Importance is replaced by task type

--- WorkLeisure/src/jneickhoff/workleisure/DueTasksArrayAdapter.java
+++ WorkLeisure/src/jneickhoff/workleisure/DueTasksArrayAdapter.java
@@ -17,7 +17,7 @@ public class DueTasksArrayAdapter extends ArrayAdapter<Task>{
 	private List<Task> values;
 	
 	public DueTasksArrayAdapter(Context context, List<Task> values) {
-		super(context, R.layout.row_claim_simple, values);
+		super(context, R.layout.row_task, values);
 		this.context = context;
 		this.values = values;
 	}
@@ -25,13 +25,36 @@ public class DueTasksArrayAdapter extends ArrayAdapter<Task>{
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent){
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View rowView = inflater.inflate(R.layout.row_task_due, parent, false);
+		View rowView = inflater.inflate(R.layout.row_task, parent, false);
+		
+		Task task = values.get(position);
 		
 		TextView txtTaskName = (TextView) rowView.findViewById(R.id.txtTaskName);
+		TextView txtTaskImportance = (TextView) rowView.findViewById(R.id.txtTaskImportance);
 		TextView txtTaskDateDue = (TextView) rowView.findViewById(R.id.txtTaskDateDue);
+		TextView txtTaskBounty = (TextView) rowView.findViewById(R.id.txtTaskBounty);
 		
-		txtTaskName.setText(values.get(position).getName());
-		txtTaskDateDue.setText(DateFormat.getDateFormat(context).format(values.get(position).getDateDue()));
+		
+		txtTaskName.setText(task.getName());
+//		if(task.getImportance().equals(Task.IMPORTANCE_HIGH)) {
+//			txtTaskImportance.setText(context.getResources().getString(R.string.high));
+//			txtTaskImportance.setBackgroundColor(context.getResources().getColor(R.color.yellow));
+//		}
+//		else if(task.getImportance().equals(Task.IMPORTANCE_LOW)) {
+//			txtTaskImportance.setText(context.getResources().getString(R.string.low));
+//			txtTaskImportance.setBackgroundColor(context.getResources().getColor(R.color.light_grey));
+//		}
+		if(task.getType().equals(Task.TYPE_WORK)) {
+			txtTaskImportance.setText(context.getResources().getString(R.string.work));
+			txtTaskImportance.setBackgroundColor(context.getResources().getColor(R.color.blue));
+		} 
+		else {
+			txtTaskImportance.setText(context.getResources().getString(R.string.leisure));
+			txtTaskImportance.setBackgroundColor(context.getResources().getColor(R.color.red));
+		}
+			
+		txtTaskDateDue.setText(" " + DateFormat.getDateFormat(context).format(task.getDateDue()));
+		txtTaskBounty.setText(String.format("%.1f", task.getBounty()));
 		
 		return rowView;
 	}
