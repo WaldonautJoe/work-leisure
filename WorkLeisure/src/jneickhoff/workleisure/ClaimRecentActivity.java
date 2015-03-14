39222a701358b608a46de04a91625a04007ebe38
WaldonautJoe <omanacent@gmail.com>
2015-03-14 06:35:26 -0500

    In ClaimRecentActivity, inflate layout instead of building using code

--- WorkLeisure/src/jneickhoff/workleisure/ClaimRecentActivity.java
+++ WorkLeisure/src/jneickhoff/workleisure/ClaimRecentActivity.java
@@ -8,23 +8,23 @@ import jneickhoff.workleisure.db.DataSource;
 import jneickhoff.workleisure.db.Task;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.text.format.DateFormat;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ClaimRecentActivity extends Activity {
 
 	private final static int MAX_METER_VALUE = 8;
+	private final static int DISPLAYED_DAYS = 7;
 	
 	private DataSource ds;
 	private LinearLayout lytRecentClaims;
 	
-	private final static int DISPLAYED_DAYS = 7;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -54,11 +54,10 @@ public class ClaimRecentActivity extends Activity {
 	 * update the displayed elements of this activity
 	 */
 	public void updateDisplay() {
-		
 		lytRecentClaims.removeAllViews();
 		
 		int groupsPos = DISPLAYED_DAYS - 1;
-		Resources res = this.getResources();
+		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		
 		Calendar targetDateBeg = Calendar.getInstance();
 		targetDateBeg.set(Calendar.HOUR_OF_DAY, 0);
@@ -77,24 +76,20 @@ public class ClaimRecentActivity extends Activity {
 		ds.open();
 		while (groupsPos >= 0) {
 			//set up claim displays and count bounties totals
+			View dayView = inflater.inflate(R.layout.days_claims, lytRecentClaims, false);
 			List<ClaimLog> daysClaims = ds.getAllClaimLogs(targetDateBeg, targetDateEnd, null, false);
 			
-			LinearLayout lytClaims = new LinearLayout(this);
-			lytClaims.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-			lytClaims.setOrientation(LinearLayout.VERTICAL);
-			lytClaims.setPadding((int) res.getDimension(R.dimen.activity_horizontal_margin), 
-								 0, 
-								 (int) res.getDimension(R.dimen.activity_horizontal_margin), 
-								 10);
+			TextView txtDateHeader = (TextView) dayView.findViewById(R.id.txtDateHeader);
+			HorizontalMeter meterWork = (HorizontalMeter) dayView.findViewById(R.id.meterWork);
+			HorizontalMeter meterLeisure = (HorizontalMeter) dayView.findViewById(R.id.meterLeisure);
+			LinearLayout lytClaims = (LinearLayout) dayView.findViewById(R.id.lytClaims);
 			
 			float workClaimedTime = 0.0f;
 			float leisClaimedTime = 0.0f;
 			
 			if(daysClaims.size() == 0) {
-				TextView txtEmptyMsg = new TextView(this);
-				txtEmptyMsg.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-				txtEmptyMsg.setText("No claims");
-				lytClaims.addView(txtEmptyMsg);
+				TextView txtEmptyMsg = (TextView) dayView.findViewById(R.id.txtNoClaims);
+				txtEmptyMsg.setVisibility(View.VISIBLE);
 			}
 			else {
 				ClaimRecentArrayAdapter claimAdapter = new ClaimRecentArrayAdapter(this, daysClaims);
@@ -102,19 +97,7 @@ public class ClaimRecentActivity extends Activity {
 					ClaimLog claim = daysClaims.get(i);
 					View view = claimAdapter.getView(i, null, null);
 					
-					view.setTag(claim.getTaskID());
-					view.setOnClickListener(new View.OnClickListener() {
-						
-						@Override
-						public void onClick(View v) {
-							long taskID = (Long) v.getTag();
-							
-							Intent intent = new Intent(ClaimRecentActivity.this, TaskDetailActivity.class);
-							intent.putExtra(TaskDetailActivity.EXTRA_TASK_ID, taskID);
-							startActivity(intent);
-						}
-					});
-					
+					view.setOnClickListener(getClaimClickListener(claim.getTaskID()));
 					lytClaims.addView(view);
 					
 					if(claim.getTaskType().equals(Task.TYPE_WORK))
@@ -124,50 +107,12 @@ public class ClaimRecentActivity extends Activity {
 				}
 			}
 			
-			//set up date header
-			TextView txtDateHeader = new TextView(this);
-			txtDateHeader.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-			txtDateHeader.setPadding((int) res.getDimension(R.dimen.activity_horizontal_margin), 
-									 0, 
-									 (int) res.getDimension(R.dimen.activity_horizontal_margin), 
-									 0);
-			txtDateHeader.setBackgroundColor(this.getResources().getColor(R.color.yellow));
-			txtDateHeader.setTextColor(this.getResources().getColor(R.color.white));
-			txtDateHeader.setTextSize(22);
 			txtDateHeader.setText(DateFormat.getDateFormat(this).format(targetDateBeg.getTime()));
 			
-			//set up bounty meters
-			LinearLayout lytBountyMeters = new LinearLayout(this);
-			lytBountyMeters.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-			lytBountyMeters.setOrientation(LinearLayout.VERTICAL);
-			lytBountyMeters.setPadding((int) res.getDimension(R.dimen.activity_horizontal_margin), 
-								 0, 
-								 (int) res.getDimension(R.dimen.activity_horizontal_margin), 
-								 10);
+			meterWork.setValue(workClaimedTime, MAX_METER_VALUE);
+			meterLeisure.setValue(leisClaimedTime, MAX_METER_VALUE);
 			
-			HorizontalMeter workMeter = new HorizontalMeter(this, 
-					workClaimedTime, MAX_METER_VALUE, false,
-					getResources().getColor(R.color.blue), 
-					getResources().getColor(R.color.blue_light2),
-					getResources().getColor(R.color.light_grey),
-					getResources().getColor(R.color.light_grey),
-					false);
-			workMeter.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-			HorizontalMeter leisMeter = new HorizontalMeter(this, 
-					leisClaimedTime, MAX_METER_VALUE, false, 
-					getResources().getColor(R.color.red), 
-					getResources().getColor(R.color.red_light2),
-					getResources().getColor(R.color.light_grey),
-					getResources().getColor(R.color.light_grey),
-					false);
-			leisMeter.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-			
-			lytBountyMeters.addView(workMeter);
-			lytBountyMeters.addView(leisMeter);
-			
-			lytRecentClaims.addView(txtDateHeader);
-			lytRecentClaims.addView(lytBountyMeters);
-			lytRecentClaims.addView(lytClaims);
+			lytRecentClaims.addView(dayView);
 			
 			//go to next day
 			targetDateBeg.add(Calendar.DAY_OF_MONTH, -1);
@@ -175,6 +120,23 @@ public class ClaimRecentActivity extends Activity {
 			groupsPos--;
 		}
 		ds.close();
+	}
+	
+	/**
+	 * Displays task detail upon click
+	 * @param taskId id of task on which to display details
+	 * @return click listener
+	 */
+	private View.OnClickListener getClaimClickListener(final long taskID) {
+		return new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(ClaimRecentActivity.this, TaskDetailActivity.class);
+				intent.putExtra(TaskDetailActivity.EXTRA_TASK_ID, taskID);
+				startActivity(intent);
+			}
+		};
 	}
 
 }
